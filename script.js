69383047428bc79b271f6468f82c978e59fa0a8d
Douglas <yogi.yokomizo@gmail.com>
2023-05-19 10:55:03 -0300

    validação criada

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-//SE  capacidadeMax > numeroPessoas && os dois campos forem preenchidos = success 
-//SE  numeroPessoas > capacidadeMax = "capacidade máxima ultrapadassada"
-//SE  os campos não forem preenchidos = "error-message"
 
-let botao = document.getElementsByTagName('button');
 const form = document.getElementById('form-confirm');
 
-function validaCapacidade(capacidadeMax, qtdPessoas){
-    capacidadeMax = document.getElementById('capacidade');
-    qtdPessoas = document.getElementById('qtd-pessoas');
-    if(qtdPessoas.value > capacidadeMax.value){
-        alert("Limite de pessoas ultrapassado")
-    } else {
-        alert("Ainda há espaço sobrando")
+form.addEventListener('submit', function(confere){
+    capacidadeMax = document.getElementById('capacidade').value;
+    qtdPessoas = document.getElementById('qtd-pessoas').value;
+    limite = document.querySelector('.limite')
+    if(qtdPessoas > capacidadeMax)
+    {
+        console.log("Limite de pessoas ultrapassado")
+        confere.preventDefault();
+        limite.style.display = "block";
+
+    } 
+    else 
+    {
+        alert("Número de pessoas aprovado!\nBom evento!")
     }
-}
-
-validaCapacidade()
-
-// form.addEventListener('submit', function(evento){
-//     evento.preventDefault();
-// })
+})
