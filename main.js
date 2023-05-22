5ae72dce92dbad4a720c576c1596cd5b696878bc
Douglas <yogi.yokomizo@gmail.com>
2023-05-22 09:56:40 -0300

    ToDo list concluido

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-$(document).ready({
+let novaTarefa = " "
+let tarefa = " "
 
-    
+$('form').on('submit', function(adicionarTarefa){
+    adicionarTarefa.preventDefault();
+
+    tarefa = $('#inserir-tarefa').val();
+    novaTarefa = $(`<li id="task"></li>`);
+    $(`<a>${tarefa}</a>`).appendTo(novaTarefa);
+    $(novaTarefa).appendTo('ul');
+
+    $('#inserir-tarefa').val('');
+    $('#inserir-tarefa').focus();
 })
+
+$('ul').on('click', 'a', function() {
+    $(this).css("text-decoration", "line-through");
+  });
