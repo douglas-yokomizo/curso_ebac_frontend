9e09a35b43c6e3303fb83658f7531e53b9306da9
Douglas <yogi.yokomizo@gmail.com>
2023-05-27 17:00:42 -0300

    exercicio módulo 16 finalizado

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+
+function comprimeImagens(){
+    return gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/image'))
+}
+
+function comprimeJavaScript(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass() {
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+exports.default = function() {
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass))
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript))
+    gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens))
+}
---
+++ src/scripts/change.js
@@ -0,0 +1,12 @@
+const buttons = {
+	prev: document.querySelector(".btn--left"),
+	next: document.querySelector(".btn--right"),
+};
+const cardsContainerEl = document.querySelector(".cards__wrapper");
+const appBgContainerEl = document.querySelector(".app__bg");
+
+const cardInfosContainerEl = document.querySelector(".info__wrapper");
+
+buttons.next.addEventListener("click", () => swapCards("right"));
+
+buttons.prev.addEventListener("click", () => swapCards("left"));
